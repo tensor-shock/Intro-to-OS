c276610087fc5e4fa0806c1bfcdde54f3bda31d9
RajdeepDas <rajdeep.ind24@gmail.com>
2022-09-29 12:38:48 +0530

    Added work of first assignment

---
+++ Assignment1/bg.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+file_path = r'/Users/rajarshidas/Desktop/rajdeep/IIT/Sem7/MTL458-OPERATING SYSTEMS/Assignment1/lol.txt'
+# if os.path.exists(file_path):
+#     print('file already exists')
+# else:
+#     # create a file
+#     for i in range(100):
+#         print(i)
+#         time.sleep(0.5)
+#     with open(file_path, 'w') as fp:
+#         # uncomment if you want empty file
+#         fp.write('This is first line')
+
+for i in range(5):
+    # i=i+1
+    print(i)
+    time.sleep(5)
