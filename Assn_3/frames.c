4a0f7166f58fefe399eabcd25af2b756a7001282
RajdeepDas <rajdeep.ind24@gmail.com>
2022-10-15 00:51:30 +0530

    added fifo random lru clock

--- Assn_3/frames.c
+++ Assn_3/frames.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// #define offset_mask 0x111;
-#define vpn_mask 0x11111000;
+int vpn_mask = 0xfffff000;
 
 char *inputfile;
 int frames;
@@ -11,9 +10,29 @@ char *method;
 char read_write;
 unsigned int v_address;
 int verb=0;
+int *page_table;
+int *valid;
+int *dirty;
+int num_mem_access=0;
+int misses=0;
+int writes=0;
+int drops=0;
 
+void print_page_table()
+{
+    for(int i=0;i<frames;i++)
+    {
+        printf("entry %d is %x\n",i,page_table[i]);
+    }
+}
 
-
+void print_summary()
+{
+    printf("Number of memory accesses:%d\n",num_mem_access);
+    printf("Number of misses:%d\n",misses);
+    printf("Number of writes:%d\n",writes);
+    printf("Number of drops:%d\n",drops);
+}
 
 void opt_str()
 {
@@ -21,31 +40,385 @@ void opt_str()
 }
 void fifo_str()
 {
-
+    
     FILE* fptr = fopen(inputfile, "r");
     if (fptr == NULL) {
         printf("File Error 404");
         return;
     }
+
+    int fifo_start_index=0;
+
     while(fscanf(fptr, "%x %c", &v_address, &read_write)>0)
     {
-        printf("%x %c\n",v_address,read_write);
-        int vpn=v_address & vpn_mask;
-        
+        // sanity
+        // printf("%x %c\n",v_address,read_write);
+
+        int vpn=(v_address & vpn_mask)>>12;
+        int present=0;
+        num_mem_access++;
+        for(int i=0;i<frames;i++)
+        {
+            if(valid[i]!=0)
+            {
+                if(page_table[i]==vpn)
+                {
+                    //hit
+                    present=1;
+                    if(read_write=='W')
+                    dirty[i]=1;
+                    break;
+                }
+            }
+        }
+
+        //miss->page replacement
+        if(!present)
+        {
+            misses++;
+            int replaced=page_table[fifo_start_index];
+            page_table[fifo_start_index]=vpn;
+
+
+            if(verb && valid[fifo_start_index])
+            {
+                printf("Page %x was read from disk, ",vpn);
+            }
+            if(dirty[fifo_start_index]==0 && valid[fifo_start_index])
+            {
+                drops++;
+                if(verb)
+                {
+                    printf("page %x was dropped (it was not dirty).\n",replaced);
+                }
+            }
+            else if(valid[fifo_start_index])
+            {
+                writes++;
+                if(verb)
+                {
+                    printf("page %x was written to the disk.\n",replaced);
+                }
+            }
+
+            valid[fifo_start_index]=1;
+            dirty[fifo_start_index]=0;
+            if(read_write=='W')
+            dirty[fifo_start_index]=1;
+
+            fifo_start_index+=1;
+            fifo_start_index%=frames;
+        }
+
+        // sanity
+        // printf("page table is \n");
+        // print_page_table();
+        // printf("\n");
+
     }
+    print_summary();
+    
 
 }
 void clock_str()
 {
-    
+    FILE* fptr = fopen(inputfile, "r");
+    if (fptr == NULL) {
+        printf("File Error 404");
+        return;
+    }
+
+    int assigned=0;
+    int *reference = malloc(frames*sizeof(int));
+    int clock_hand=0;
+    for(int i=0;i<frames;i++)
+    {
+        reference[i]=0;
+    }
+
+    while(fscanf(fptr, "%x %c", &v_address, &read_write)>0)
+    {
+        // sanity
+        // printf("%x %c\n",v_address,read_write);
+        
+        int vpn=(v_address & vpn_mask)>>12;
+        int present=0;
+        num_mem_access++;
+        for(int i=0;i<frames;i++)
+        {
+            if(valid[i]!=0)
+            {
+                if(page_table[i]==vpn)
+                {
+                    //hit
+                    present=1;
+                    if(read_write=='W')
+                    dirty[i]=1;
+                    reference[i]=1;
+                    break;
+                }
+            }
+        }
+
+        //miss->page replacement
+        if(!present && assigned<frames)
+        {
+            misses++;
+            page_table[assigned]=vpn;
+            valid[assigned]=1;
+            reference[assigned]=1;
+            assigned++;
+        }
+        else if(!present)
+        {
+            misses++;
+
+            int replaced_index;
+            int got_index=0;
+            
+            for(int i=0;i<=frames;i++)
+            {
+                if(reference[clock_hand]==0)
+                {
+                    replaced_index=clock_hand;
+                    got_index=1;
+                }
+                else{
+                    reference[clock_hand]=0;
+                }
+                clock_hand+=1;
+                clock_hand%=frames;
+                if(got_index==1)
+                break;
+            }
+            int replaced=page_table[replaced_index];
+
+
+            if(verb)
+            {
+                printf("Page %x was read from disk, ",vpn);
+            }
+            if(dirty[replaced_index]==0)
+            {
+                drops++;
+                if(verb)
+                {
+                    printf("page %x was dropped (it was not dirty).\n",replaced);
+                }
+            }
+            else
+            {
+                writes++;
+                if(verb)
+                {
+                    printf("page %x was written to the disk.\n",replaced);
+                }
+            }
+            page_table[replaced_index]=vpn;
+            reference[replaced_index]=1;
+            valid[replaced_index]=1;
+            dirty[replaced_index]=0;
+            if(read_write=='W')
+            dirty[replaced_index]=1;
+        }
+
+        // sanity
+        // printf("page table is \n");
+        // print_page_table();
+        // printf("\n");
+
+    }
+    print_summary();
 }
 void lru_str()
 {
-    
+    FILE* fptr = fopen(inputfile, "r");
+    if (fptr == NULL) {
+        printf("File Error 404");
+        return;
+    }
+
+    int assigned=0;
+    int counts=0;
+    int *recent_array = malloc(frames*sizeof(int));
+    for(int i=0;i<frames;i++)
+    {
+        recent_array[i]=-1;
+    }
+
+    while(fscanf(fptr, "%x %c", &v_address, &read_write)>0)
+    {
+        // sanity
+        // printf("%x %c\n",v_address,read_write);
+        
+        int vpn=(v_address & vpn_mask)>>12;
+        int present=0;
+        num_mem_access++;
+        for(int i=0;i<frames;i++)
+        {
+            if(valid[i]!=0)
+            {
+                if(page_table[i]==vpn)
+                {
+                    //hit
+                    present=1;
+                    if(read_write=='W')
+                    dirty[i]=1;
+                    recent_array[i]=counts;
+                    counts++;
+                    break;
+                }
+            }
+        }
+
+        //miss->page replacement
+        if(!present && assigned<frames)
+        {
+            misses++;
+            page_table[assigned]=vpn;
+            valid[assigned]=1;
+            recent_array[assigned]=counts;
+            assigned++;
+            counts++;
+        }
+        else if(!present)
+        {
+            misses++;
+
+            int replaced_index = 0;
+            
+            for(int i=1;i<frames;i++)
+            {
+                if(recent_array[i]<recent_array[replaced_index])
+                {
+                    replaced_index=i;
+                }
+            }
+            int replaced=page_table[replaced_index];
+
+
+            if(verb)
+            {
+                printf("Page %x was read from disk, ",vpn);
+            }
+            if(dirty[replaced_index]==0)
+            {
+                drops++;
+                if(verb)
+                {
+                    printf("page %x was dropped (it was not dirty).\n",replaced);
+                }
+            }
+            else
+            {
+                writes++;
+                if(verb)
+                {
+                    printf("page %x was written to the disk.\n",replaced);
+                }
+            }
+            page_table[replaced_index]=vpn;
+            recent_array[replaced_index]=counts;
+            valid[replaced_index]=1;
+            dirty[replaced_index]=0;
+            if(read_write=='W')
+            dirty[replaced_index]=1;
+
+            counts++;
+        }
+
+        // sanity
+        // printf("page table is \n");
+        // print_page_table();
+        // printf("\n");
+
+    }
+    print_summary();
 }
 void random_str()
 {
-    
+    FILE* fptr = fopen(inputfile, "r");
+    if (fptr == NULL) {
+        printf("File Error 404");
+        return;
+    }
+
+    srand(5635);
+    int assigned=0;
+    while(fscanf(fptr, "%x %c", &v_address, &read_write)>0)
+    {
+        // sanity
+        // printf("%x %c\n",v_address,read_write);
+        
+        int vpn=(v_address & vpn_mask)>>12;
+        int present=0;
+        num_mem_access++;
+        for(int i=0;i<frames;i++)
+        {
+            if(valid[i]!=0)
+            {
+                if(page_table[i]==vpn)
+                {
+                    //hit
+                    present=1;
+                    if(read_write=='W')
+                    dirty[i]=1;
+                    break;
+                }
+            }
+        }
+
+        //miss->page replacement
+        if(!present && assigned<frames)
+        {
+            misses++;
+            page_table[assigned]=vpn;
+            valid[assigned]=1;
+            assigned++;
+        }
+        else if(!present)
+        {
+            misses++;
+
+            int replaced_index = rand() % frames;
+            int replaced=page_table[replaced_index];
+
+
+            if(verb)
+            {
+                printf("Page %x was read from disk, ",vpn);
+            }
+            if(dirty[replaced_index]==0)
+            {
+                drops++;
+                if(verb)
+                {
+                    printf("page %x was dropped (it was not dirty).\n",replaced);
+                }
+            }
+            else
+            {
+                writes++;
+                if(verb)
+                {
+                    printf("page %x was written to the disk.\n",replaced);
+                }
+            }
+            page_table[replaced_index]=vpn;
+            valid[replaced_index]=1;
+            dirty[replaced_index]=0;
+            if(read_write=='W')
+            dirty[replaced_index]=1;
+        }
+
+        // sanity
+        // printf("page table is \n");
+        // print_page_table();
+        // printf("\n");
+
+    }
+    print_summary();
+
 }
 
 
@@ -86,7 +459,16 @@ int main(int argc,char **argv)
         verb=1;
     }
 
-    //getting strategy
+    page_table=malloc(frames*sizeof(int));
+    valid=malloc(frames*sizeof(int));
+    dirty=malloc(frames*sizeof(int));
+    for(int i=0;i<frames;i++)
+    {
+        valid[i]=0;
+        dirty[i]=0;
+    }
+
+    //doing strategy
     do_strategy(method);
 
     return 0;
